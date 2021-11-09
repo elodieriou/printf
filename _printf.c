550629aef7df4b949bf8d78c7e52a77b68888a23
julien3641 <3641@holbertonschool.com>
2021-11-09 17:35:23 +0100

    add file _printf.c

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _printf - function that print with outpu format
+ * @format: is a character string
+ * Return: the number of caracters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	format_t list[] = {
+		{"c", print_char}, {"s", print_string}, {"%", print_percent},
+		{NULL, NULL}};
+	int i, j, count = 0, len = 0;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
+	va_start(args, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			count += 1;
+			_putchar (format[i]);
+		}
+		else
+		{
+			for (j = 0; list[j].id != NULL; j++)
+			{
+				if (format[i + 1] == *list[j].id)
+				{
+					len = list[j].f(args);
+					count += len;
+				}
+			}
+			i++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
