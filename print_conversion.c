27cf5760801c7e7a9d8062ec409a0b92abe590aa
Elodie Riou <elodie.riou@hotmail.com>
2021-11-09 17:20:53 +0100

    add print_conversion.c

---
+++ print_conversion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_char - function that print a char
+ * @args: variadic arguments
+ * Return: 1
+ */
+int print_char(va_list args)
+{
+	int n;
+
+	n = va_arg(args, int);
+	_putchar (n);
+	return (1);
+}
+
+/**
+ * print_string - function that print a string
+ * @args: variadic arguments
+ * Return: the string
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i;
+
+	if (str == NULL)
+		str = "(nil)";
+
+	for (i = 0; str[i]; i++)
+		_putchar (str[i]);
+	return (i);
+}
+
+/**
+ * print_percent - function that print a percent
+ * @args: variadic arguments
+ * Return: a percent
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar ('%');
+	return (1);
+}
